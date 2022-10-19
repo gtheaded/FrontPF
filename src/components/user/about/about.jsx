b214ca766874c1f344cb3682749f62e3114c6053
gtheaded <gaston.turner@gmail.com>
2022-10-19 20:42:35 -0300

    new deployment soon

--- src/components/user/about/about.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const About = () => {
-  return <div>Estas en / about</div>;
-};
-
-export default About;
--- src/components/user/profile/shopping.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Shopping = () => {
-    return ( 
-        <div className='z-10 absolute bottom-0'><h1>Holaaaaaaaaaaaaaaaa</h1></div>
-     );
-}
- 
-export default Shopping;
